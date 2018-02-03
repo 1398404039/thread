1f9051115089abaeaefc8d4391240628cce38beb
lwz <1398404039@qq.com>
2018-02-03 09:05:27 +0800

    新建
    新建

---
+++ out/production/thread/main/concurrent/readme.txt
@@ -0,0 +1,6 @@
+
+本包下，简单介绍一下java并发开发框架
+1、Fork/Join
+2、CountDownLauth
+
+的使用Demo演示
--- src/main/threadPool/ThreadPoolExecutorDemo.java
+++ src/main/threadPool/ThreadPoolExecutorDemo.java
@@ -1,8 +1,34 @@
 package main.threadpool;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 /**
  * @author: 0
  * @create: 2018-01-29 14:42
  * @description: 通过ThreadPoolExecutor来创建线程池
  **/
 public class ThreadPoolExecutorDemo {
+    private static LocalDate date ;
+    private static LocalTime time ;
+
+    public static void main(String[] args){
+        System.out.println(date.atStartOfDay());
+//        System.out.println(date.);
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
 }
